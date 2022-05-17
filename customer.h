fbf87268d6568208dbfd3bd062c81efca4b7a4e2
Kaveesha  Dahanayake <99637419+IT21232404@users.noreply.github.com>
2022-05-17 12:04:56 +0000

    added the header file of customer

---
+++ customer.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include "Customer.h"
+using namespace std;
+
+Customer::Customer(string Fname, string Lname, int ZipCode, string contact, string Email, int age, string country, string DOB) {
+    this->Fname = Fname;
+    this->Lname = Lname;
+    this->ZipCode = ZipCode;
+    this->contact = contact;
+    this->Email = Email;
+    this->age = age;
+    this->country = country;
+    this->DOB = DOB;
+
+}
+
+Customer::~Customer()
+{
+    cout<<"Customer object destroyed"<<endl;
+}
+
