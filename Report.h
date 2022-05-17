260bcf75665fe3a0994075fb3713f637d0b7c310
Kaveesha  Dahanayake <99637419+IT21232404@users.noreply.github.com>
2022-05-17 15:40:59 +0000

    Header file of application

---
+++ Report.h
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <iostream>
+using namespace std;
+class Report
+{
+private:
+    int RefId;
+    string Name;
+    string Title;
+    string Date;
+    string type;
+
+
+public:
+    Report(int RefId, string Name, string Title, string Date, string type);
+    ~Report();
+};
+
