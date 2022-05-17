e5c5aead8556e573167d29e5e20f832c07a62042
Kaveesha  Dahanayake <99637419+IT21232404@users.noreply.github.com>
2022-05-17 17:20:06 +0000

    Added the cpp file of application class

---
+++ Application.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+#include "Application.h"
+using namespace std;
+
+Application::Application(int Id, string Name, string Category, string Type, string Status)
+{
+    this->Id = Id;
+    this->Name = Name;
+    this->Category = Category;
+    this->Type = Type;
+    this->Status = Status;
+}
+
+Application::~Application()
+{
+}
+
---
+++ Report.cpp
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <iostream>
+#include "Report.h"
+using namespace std;
+
+Report::Report(int RefId, string Name, string Title, string Date, string type) {
+    this->RefId = RefId;
+    this->Name = Name;
+    this->Title = Title;
+    this->Date = Date;
+    this->type = type;
+    
+}
+
+Report::~Report()
+{
+}
