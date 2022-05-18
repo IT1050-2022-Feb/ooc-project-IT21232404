548d2f9d0fe0b49fbc113c70706af8ce3dde4911
Kaveesha  Dahanayake <99637419+IT21232404@users.noreply.github.com>
2022-05-18 04:05:39 +0000

    Added Feedback cpp file

---
+++ Feedback.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+#include "Feedback.h"
+using namespace std;
+
+Feedback::Feedback(int refId, double rating, string comment, string email)
+{
+    this->refId = refId;
+    this->rating = rating;
+    this->comment = comment;
+    this->email = email;
+}
+
+Feedback::~Feedback()
+{
+}
